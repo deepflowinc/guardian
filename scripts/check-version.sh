2cf36da4d35ccceb87d3a6fc579dccdc885281ba
Hiromi ISHII <konn.jinro@gmail.com>
2023-02-09 13:19:22 +0900

    ci: checks cabal version in Release

---
+++ scripts/check-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+RELEASE=${1}
+
+cabal update; cabal build --dry-run
+VERSION="$(jq -r '.["install-plan"][] | select(.["pkg-name"] == "guardian") | .["pkg-version"]' dist-newstyle/cache/plan.json | uniq)"
+
+if [[ "${VERSION}" = "${RELEASE}" ]]; then
+  echo "Release version ok: ${VERSION}"
+else
+  echo "Release version mismatched!" >&2
+  echo "   expected: ${RELEASE}" >&2
+  echo "  cabal ver: ${VERSION}" >&2
+  exit 1
+fi
