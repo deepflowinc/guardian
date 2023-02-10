dab0e3f558e218a97df04aef2b8e059269d33b9d
Hiromi ISHII <konn.jinro@gmail.com>
2023-02-10 17:47:11 +0900

    action: avoid using gh, as it requires GH_TOKEN

--- action/download-guardian.sh
+++ action/download-guardian.sh
@@ -15,7 +15,7 @@ popd () {
 
 [ -z "${1}" ] && log "Usage: download-guardian.sh (RELEASE) [DOWNLOAD_DIR]" && exit 1
 
-set -euxo pipefail
+set -euo pipefail
 RELEASE="${1}"
 DOWNLOAD_PATH="${2:-guardian-$(date +%Y%m%d-%H%M%S)}"
 
@@ -30,23 +30,25 @@ case "${UNAME}" in
     exit 1;
 esac
 
-GH="gh -R deepflowinc/guardian"
 if [ "${RELEASE}" = "latest" ]; then
-  RELEASE=$(${GH} release list -L1 | cut -f1)
+  PAYLOAD=$(curl https://api.github.com/repos/deepflowinc/guardian/releases/latest)
+else
+  PAYLOAD=$(curl "https://api.github.com/repos/deepflowinc/guardian/releases/tags/v${RELEASE}")
 fi
 
-if ${GH} release view "v${RELEASE}"; then
+if RELEASE="$(echo "${PAYLOAD}" | jq -r ".name")"; then
   log "Downloading guardian version: ${RELEASE}..."
   mkdir -p "${DOWNLOAD_PATH}"
   TARBALL="guardian-${RELEASE}-x86_64-${OS}.tar.gz"
   pushd "${DOWNLOAD_PATH}"
-    ${GH} release download >/dev/null 2>&1 \
-      "v${RELEASE}" -p "${TARBALL}" -p  "SHA256SUMS" -p "SHA256SUMS.asc"
+    echo "${PAYLOAD}" | jq -r '.assets | map(.browser_download_url) | .[]' | while read -r URL; do
+      curl --location --remote-name "${URL}" >/dev/null 2>&1
+    done
     log "Downloaded."
     # shared runner lacks GPG...
     # gpg --verify SHA256SUMS.asc >/dev/null 2>&1
     log "Checking SHA256 sums..."
-    sha256sum --check --ignore-missing SHA256SUMS  >/dev/null 2>&1
+    sha256sum --check --ignore-missing SHA256SUMS >/dev/null 2>&1
     tar xzf "${TARBALL}"
     rm ./*.tar.gz ./SHA256SUMS*
   popd
